db566003988100deba3407e6d7a5550f7afafcf4
tayleredelen <taylerledelen@gmail.com>
2023-10-10 10:22:22 -0500

    Day 22: added app description, image, and links

--- main.py
+++ main.py
@@ -24,16 +24,20 @@ Feel free to contact me!
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_column, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep=";")
 # tells pandas to read csv and separate data by semicolon
 
 with col3:
     for index, row in df.iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
 with col4:
     for index, row in df.iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
