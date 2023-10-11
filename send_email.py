8c3df88fe5b65a79fa3ca7c386a9214f3798ad80
tayleredelen <taylerledelen@gmail.com>
2023-10-11 13:36:07 -0500

    Day 23: updated environmental variables

--- send_email.py
+++ send_email.py
@@ -1,11 +1,11 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "taylerledelen@gmail.com"
-    password = "kggmqgkfbqzkgcfm"
+    password = os.getenv("PASSWORD")
     receiver = "taylerledelen@gmail.com"
     context = ssl.create_default_context()
 
