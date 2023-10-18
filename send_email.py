84bcf76f0d4e857a21c26c088628baf2af9889b3
tayleredelen <taylerledelen@gmail.com>
2023-10-18 12:34:34 -0500

    Added Positive News Site github repo link

--- send_email.py
+++ send_email.py
@@ -1,6 +1,7 @@
 import smtplib, ssl
 import os
 
+
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
@@ -12,5 +13,3 @@ def send_email(message):
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-
